1823281e5826e92a9161678eb7d121a2e32d602d
Riana Battick <rianabattick@Rianas-Air.localdomain>
2025-05-01 18:17:41 -0400

    set api key in app and add gitignore file

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 import json
 import os
 import openai
+from dotenv import load_dotenv
+load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
